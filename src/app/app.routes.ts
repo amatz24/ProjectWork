a46f0b3d924dcc580178c58f238847ad789e806e
Antonio Russo <antonyrusso7o76@gmail.com>
2025-02-11 14:14:32 +0100

    commit prova-burger

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { BurgersComponent } from './components/burgers/burgers.component';
 
 export const routes: Routes = [
+    {path:'burg',component: BurgersComponent}
     
 ];
