2c777d8033c07bcd2df7510823eb98b736da4874
Matteo Caroli <vaiabbjj@gmail.com>
2025-02-11 14:34:29 +0100

    Commit 6 - routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { DrinksComponent } from './components/drinks/drinks.component';
 import { BurgersComponent } from './components/burgers/burgers.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: "drinks", component: DrinksComponent },
     { path: 'burg', component: BurgersComponent }
 
