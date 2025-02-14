0122d189575959e61a4cac7ca5e76c0228ca8e43
DiegoRandazzo-scuola <diegorandazzo69@gmail.com>
2025-02-14 11:26:51 +0100

    aggiunta descrizione about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -16,7 +16,7 @@ export class AboutComponent {
     { nome: 'Antonio Russo', img: 'assets/antonio.jpeg', descrizione: "Nel progetto, mi sono occupato dell'implementazione della pagina 'Burger', del carrello e delle pagine finali 'Paga' e 'Annulla Ordine', oltre alla pagina 'NotFound'. Inoltre, ho sviluppato i relativi componenti, modelli e servizi in Angular e ho gestito la visualizzazione delle quantità degli ingredienti nel carrello" },
     { nome: 'Riccardo Signoretti', img: 'assets/ricky.jpeg', descrizione: '' },
     { nome: 'Matteo Caroli', img: 'assets/matteo.jpeg', descrizione: 'Inizialmente, ho creato un repository pubblico su GitHub per permettere ai miei collaboratori di accedere al progetto. Successivamente, ho generato il file Angular e scritto i comandi necessari per creare tutti i componenti e i servizi.  Una volta completata questa fase, mi sono dedicato alla realizzazione della pagina dei drink, sviluppando il file TypeScript, lo stile e l`HTML. In seguito, ho lavorato sulla funzionalità di modifica degli ingredienti del panino, occupandomi in parte anche dell`aggiornamento del prezzo modificato all`interno del carrello.' },
-    { nome: 'Diego Randazzo', img: 'assets/diego.jpeg', descrizione: '' }
+    { nome: 'Diego Randazzo', img: 'assets/diego.jpeg', descrizione: 'Nel progetto mi sono dedicato alle pagine dei contorni e dei caffè/dolci. Mi sono anche occupato dello stile della finestra di modifica degli ingredienti dei panini e delle checkbox nella pagina dei drink oltre ad aver perfezionato l incremento del prezzo alla aggiunta degli ingredienti e la grandezza degli elementi nelle pagine burger, drink, dessert e side dish' }
   ];
 
   info(persona: any) {
