31308c600c48443acc278062a8c64771fa2a1c6f
Antonio Russo <antonyrusso7o76@gmail.com>
2025-02-14 11:14:32 +0100

    commit di rimozione errori

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -13,7 +13,7 @@ export class AboutComponent {
   selectedPerson: any = null;
 
   persone = [
-    { nome: 'Antonio Russo', img: 'assets/antonio.jpeg', descrizione: '' },
+    { nome: 'Antonio Russo', img: 'assets/antonio.jpeg', descrizione: "Nel progetto, mi sono occupato dell'implementazione della pagina 'Burger', del carrello e delle pagine finali 'Paga' e 'Annulla Ordine', oltre alla pagina 'NotFound'. Inoltre, ho sviluppato i relativi componenti, modelli e servizi in Angular e ho gestito la visualizzazione delle quantità degli ingredienti nel carrello" },
     { nome: 'Riccardo Signoretti', img: 'assets/ricky.jpeg', descrizione: '' },
     { nome: 'Matteo Caroli', img: 'assets/matteo.jpeg', descrizione: '' },
     { nome: 'Diego Randazzo', img: 'assets/diego.jpeg', descrizione: '' }
